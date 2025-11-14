b410a9e58e5bb5b3902d82191800a241a23b0e1b
Pedro-Soares09 <pedrosoares4525@gmail.com>
2025-11-14 18:11:35 -0300

    Update questao 23.c

--- Atividades em C/questao 23.c
+++ Atividades em C/questao 23.c
@@ -8,7 +8,7 @@ int main()
 
     float a, b, c;
 
-    printf("--- Calculadora de Equacao do 2o Grau ---\n");
+    printf("--- Calculadora de Equacao do 2ª Grau ---\n");
     printf("Digite o valor de a: ");
     scanf("%f", &a);
 
@@ -25,15 +25,15 @@ int main()
         float delta = (b * b) - 4 * a * c;
 
         if (delta < 0) {
-            printf("Nao ha solucoes reais, pois o discriminante (delta) é negativo.\n");
+            printf("Nao a solucoes reais, pois o discriminante (delta) é negativo.\n");
         } else if (delta == 0) {
             float x1 = -b / (2 * a);
-            printf("Ha duas solucoes iguais:\n");
+            printf("a duas solucoes iguais:\n");
             printf("x1 = x2 = %.2f\n", x1);
         } else { 
             float x1 = (-b + sqrt(delta)) / (2 * a);
             float x2 = (-b - sqrt(delta)) / (2 * a);
-            printf("Ha duas solucoes reais e diferentes:\n");
+            printf("a duas solucoes reais e diferentes:\n");
             printf("x1 = %.2f\n", x1);
             printf("x2 = %.2f\n", x2);
         }
@@ -41,3 +41,4 @@ int main()
 
     return 0;
 }
+
