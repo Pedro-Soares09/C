681d299961764a95fb8fd5c5aeb1141e64cef1b0
Pedro-Soares09 <pedrosoares4525@gmail.com>
2025-11-14 16:47:23 -0300

    Update questao 57.c

--- Atividades em C/questao 57.c
+++ Atividades em C/questao 57.c
@@ -5,7 +5,7 @@
 
 int main()
 {
-    setlocale(LC_ALL, "Portuguese");
+    setlocale(LC_ALL, "");
     
     int vetorA[TAMANHO_DO_VETOR] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, vetorB[TAMANHO_DO_VETOR] = {10, 9, 8 , 7, 6, 5, 4, 3, 2, 1}, vetorC[TAMANHO_DO_VETOR * 2];
     int i, j = 0;
