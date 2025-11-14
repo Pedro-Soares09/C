69a7c6807509cd68ed748047e46c58499ad448db
Pedro-Soares09 <pedrosoares4525@gmail.com>
2025-11-14 10:30:55 -0300

    Create questao 58.c

---
+++ Atividades em C/questao 58.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <locale.h>
+#define TAMANHO_DO_VETOR 10
+
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+    
+    int vetorA[TAMANHO_DO_VETOR] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, vetorB[TAMANHO_DO_VETOR];
+    int i, j = 0;
+
+    for(i = TAMANHO_DO_VETOR - 1; i >= 0; i--, j++){
+        vetorB[j] = vetorA[i];
+    }
+
+    for(i = 0; i < TAMANHO_DO_VETOR; i++){
+        printf("%d ", vetorB[i]);
+    }
+
+    return 0;
+}
